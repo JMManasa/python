b6aac4318e3e8f61442808766280fef163a45b8f
JMManasa <jmanasa079@gmail.com>
2024-08-29 15:19:36 +0530

    Add files via upload

---
+++ elif (dad's gift).py
@@ -0,0 +1,13 @@
+#elif 
+#dad's gift according to result
+marks=float(input("Father: How much marks you got in exam ?\nSon : "))
+if(marks>89):  
+ print("Father: Excellent, Now I'll give you iPhone")  
+elif(marks>79):  
+ print("Father: Very Good,I'll give you Laptop")  
+elif(marks>69):  
+ print("Father: Good,I'll give you Android Phone")  
+elif(marks>59):  
+ print("Father: Nice,I'll give you Nokia-1100")  
+else:  
+ print("Father: Son you are a waste fellow !") 
